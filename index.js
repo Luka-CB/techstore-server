00d0d42d94964d02135e9ff3078868e583132fe4
Luka-CB <lukaaslamazashvili20@gmail.com>
2023-04-04 15:55:10 +0400

    Some Changes

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(
   cookieSession({
     name: "teckstorePassportCookie",
